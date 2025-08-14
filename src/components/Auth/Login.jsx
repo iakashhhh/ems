52ba2c9a666448da36da62a298277424b54d160f
Akash Sharma <128278253+iakashhhh@users.noreply.github.com>
2025-08-14 13:18:11 +0530

    Update Login.jsx

--- src/components/Auth/Login.jsx
+++ src/components/Auth/Login.jsx
@@ -37,7 +37,7 @@ const Login = ({handleLogin}) => {
           value={email}
           onChange={(e) => setEmail(e.target.value)}
           required 
-          className='outline-none bg-transparent border-2 border-emerald-600 font-medium text-lg py-2 px-6 rounded-full placeholder:text-gray-400 w-full'
+          className='outline-none bg-transparent text-gray-900 caret-emerald-600 border-2 border-emerald-600 font-medium text-lg py-2 px-6 rounded-full placeholder:text-gray-400 w-full'
           type="email"
           placeholder='Enter your email' 
         />
